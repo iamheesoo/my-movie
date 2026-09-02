79670f8cec55c74e9ebdb7a4e91f52e965b08983
iamheesoo <beforeheesoo@gmail.com>
2026-09-02 22:44:22 +0900

    feat: hilt 초기화

--- app/src/main/java/com/heesoo/my_movie/MainActivity.kt
+++ app/src/main/java/com/heesoo/my_movie/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.heesoo.my_movie.ui.theme.MymovieTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
